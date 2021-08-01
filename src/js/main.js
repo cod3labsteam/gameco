220d876a0d2194e53cd0044652a7186352f0b1f1
Alexandru Trandafir <alxetd@gmail.com>
2021-08-01 17:40:44 +0300

    Add slick-carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,39 @@
 // Load Bootstrap JS
 import bootstrap from 'bootstrap'
+import slick from 'slick-carousel'
 
 // Load Styles
 import '../scss/main.scss';
 
 // App code
 console.log(`Hello ${process.env.HELLO}`);
+
+$(".our-team__members").slick({
+
+    // normal options...
+    infinite: false,
+
+    // the magic
+    responsive: [{
+
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            infinite: true
+        }
+
+    }, {
+
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            dots: true
+        }
+
+    }, {
+
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+
+    }]
+});
