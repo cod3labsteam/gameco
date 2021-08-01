a1a9643a605b9bbd8cd63b90c347383ae0fdc775
Liviu Trandafir <liviutrandafir21@gmail.com>
2021-08-01 21:24:09 +0300

    Style footer

--- src/js/main.js
+++ src/js/main.js
@@ -9,32 +9,4 @@ import '../scss/main.scss';
 // App code
 console.log(`Hello ${process.env.HELLO}`);
 
-$('.our-team__members').slick({
-
-    // normal options...
-    infinite: false,
-
-    // the magic
-    responsive: [{
-
-        breakpoint: 1024,
-        settings: {
-            slidesToShow: 3,
-            infinite: true
-        }
-
-    }, {
-
-        breakpoint: 600,
-        settings: {
-            slidesToShow: 2,
-            dots: true
-        }
-
-    }, {
-
-        breakpoint: 300,
-        settings: "unslick" // destroys slick
-
-    }]
-});
+$('.our-team__members').slick();
