93f945fe22fda0b2343b63fc93aeb1ae87241b42
Liviu Trandafir <liviutrandafir21@gmail.com>
2021-08-02 16:11:21 +0300

    Finish responsive

--- src/js/main.js
+++ src/js/main.js
@@ -16,5 +16,24 @@ $('.team__slider').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 570,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
